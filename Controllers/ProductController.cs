412dd66ff067581ccc5da7d0af995fbc0d4b4f98
ivanv-lab <ivanvozisov33@yandex.ru>
2024-09-20 16:51:13 +0300

    Начал Product

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using BuildingShopFront.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BuildingShopFront.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public ProductController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var response =await _httpClient.GetAsync("api/Product");
+            response.EnsureSuccessStatusCode();
+            var content=await response.Content.ReadAsStringAsync();
+            var data=JsonConvert.DeserializeObject
+                <List<ProductController>>(content);
+            return View(data);
+        }
+    }
+}
